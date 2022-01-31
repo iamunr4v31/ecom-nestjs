d5475c17ec23143aa09bc2fb144fe2c195d58e00
Ashwin <ashwins1211@gmail.com>
2022-01-31 12:56:51 +0530

    added environment variables and mongoose support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
+config();
+
 @Module({
   imports: [
     ProductsModule,
     MongooseModule.forRoot(
-      'mongodb+srv://iunravel:qwerty1234@cluster0.o6mjx.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.o6mjx.mongodb.net/nestjs-demo?retryWrites=true&w=majority`,
     ),
   ],
   controllers: [AppController],
